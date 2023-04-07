373a30b23c6cc6ab2374afe8ab992fe5c49fc4a6
1sh4sk <ayeshask.24a.mailme@gmail.com>
2023-04-07 18:20:08 +0530

    added about specialist page and responsiveness

--- src/components/AboutSpecialist.jsx
+++ src/components/AboutSpecialist.jsx
@@ -1,12 +1,57 @@
 import React from "react";
 import Header from "./Header";
 
-function AboutSpecialist(){
-    return(
-        <div>
+function AboutSpecialist() {
+    return (
+        <section>
             <Header />
-            <h1>hello</h1>
-        </div>
+            <div className="about-container">
+                <div className="background-container">
+                    <h1>
+                        DR. JAYAKUMAR
+                    </h1>
+                    <p>
+                        pulmonologist, sleep specialist, allergist/immunologist, Intensivist and Infectious disease specialist
+                    </p>
+                </div>
+            </div>
+
+            <div className="content">
+                <p>
+                    Dr. Jayakumar is one of the reputed pulmonologist, sleep specialist, allergist/immunologist, Intensivist and Infectious disease specialist with a thriving expirence of more than 10 years. He is specialised in diagnosing and treating lung conditions like Asthma, Bronchiectasis, Acute respiratory distress syndrome, COPD, Cor pulmonale, Sleep Apnoea. The doctor has exemplary experience in treating Breathlessness, Chest pain, Haemoptysis, Pleural effusion, Chronic cough, Respiratory infection. He has the best treatment in Eosinophilic lung disease, Pneumothorax, Pulmonary hypertension, Sarcoidosis, Pulmonary thromboembolic disease, Upper Airway diseases. His supportive care of treatment includes Smoking cessation, Pulmonary rehabilitation, Oxygen therapy, Non invasive ventilation, Inhalers and nebulizers, Immunosuppressive drugs. Dr. Jayakumar maintains a patient centered approach and believes in delivering quality care. He pays close attention to each patient issues in order to comprehend their worries and personalise treatment to their choices. He has done highest number of practical procedures. He hails from Neyveli having completed his DTCD in Madras Medical College and DNB from Apollo Main Hospital Chennai.
+                </p>
+            </div>
+
+            <div className="content">
+                <h1 className="heading">
+                    Areas Of Expertise
+                </h1>
+
+                <div className="aoe-container">
+                
+                    <ul className="aoe-items col-lg-6 col-md-6 col-sm-12">
+                        <li><span className="icon"></span>&emsp;Asthma</li>
+                        <li><span className="icon"></span>&emsp;Bronchiectasis</li>
+                        <li><span className="icon"></span>&emsp;COPD</li>
+                        <li><span className="icon"></span>&emsp;Acute Respiratory Distress Syndrome</li>
+                        <li><span className="icon"></span>&emsp;Eosinophilic Lung Disease</li>
+                        <li><span className="icon"></span>&emsp;Cor Pulmonale</li>
+                        <li><span className="icon"></span>&emsp;Asbestos and Lung</li>
+                        <li><span className="icon"></span>&emsp;Hypersensitivity Pneumonitis</li>
+                    </ul>
+                    <ul className="aoe-items col-lg-6 col-md-6 col-sm-12">
+                        <li><span className="icon"></span>&emsp;Pleural Effusion</li>
+                        <li><span className="icon"></span>&emsp;Pneumothorax</li>
+                        <li><span className="icon"></span>&emsp;Pulmonary Hypertension</li>
+                        <li><span className="icon"></span>&emsp;Pulmonary Thromboembolic disease</li>
+                        <li><span className="icon"></span>&emsp;Respiratory Infection (Bacterial,Fungal,Viral)</li>
+                        <li><span className="icon"></span>&emsp;Sarcoidosis</li>
+                        <li><span className="icon"></span>&emsp;Sleep Apnoea and Hypo ventilation</li>
+                        <li><span className="icon"></span>&emsp;Upper Airway Diseases</li>
+                    </ul>
+                </div>
+            </div>
+        </section>
     );
 }
 
